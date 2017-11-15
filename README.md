71715d1cc7e2434af21bde0da50d1cbc4c7e5ff9
hezzysmith <smith.heather@bellaliant.net>
2017-11-15 09:47:06 -0400

    Initial Release

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # twitterChallenge
 Salesforce Twitter Challenge
+
+
+# To start the Backend server
+```
+cd server
+./mvnw spring-boot:run -e
+```
+
+# To start the Frontend application
+```
+cd client
+
+/* install http-server  (https://github.com/indexzero/http-server) */ 
+http-server
+```
---
+++ server/src/main/java/hello/TweetController.java
@@ -0,0 +1,31 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import twitter4j.*;
+
+@RestController
+public class TweetController {
+
+    public List<Status> statusList = null;
+    
+    @CrossOrigin(origins = "http://localhost:8081")
+    @RequestMapping("/tweets")
+    public List<Status> tweets() {
+        
+    	Twitter twitter = new TwitterFactory().getInstance();
+    	try {
+      		Paging paging = new Paging(1, 10);
+    		statusList = twitter.getUserTimeline("@salesforce",paging);
+    		
+        } catch (TwitterException te) {
+    		System.out.println("Failed to search tweets: " + te.getMessage());
+    	}
+        return statusList;
+    }
+}
